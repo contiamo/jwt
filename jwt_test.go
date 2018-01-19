be4c233349dda48ff92c94f5a0da2615131ec10b
Tino Rusch <tino.rusch@gmail.com>
2018-01-19 13:29:05 +0100

    added some testcases to lift up the coverage.

--- jwt_test.go
+++ jwt_test.go
@@ -1,10 +1,14 @@
 package jwt_test
 
 import (
+	"io/ioutil"
+	"net/http"
+	"reflect"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	. "github.com/contiamo/idp/jwt"
+	. "github.com/contiamo/jwt"
 )
 
 var _ = Describe("JWT", func() {
@@ -22,7 +26,150 @@ var _ = Describe("JWT", func() {
 
 	It("should be possible to create and verify a token using RSA", func() {
 		claims := Claims{"foo": "bar"}
-		pubKey, _ := ParsePublicKey([]byte(`-----BEGIN CERTIFICATE-----
+		pubKey, err := ParsePublicKey(rsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(rsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(token).NotTo(BeEmpty())
+		reClaims, err := ValidateToken(token, pubKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reClaims).To(Equal(claims))
+	})
+
+	It("should be possible to create and verify a token using ECDSA", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey, err := ParsePublicKey(ecdsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(ecdsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(token).NotTo(BeEmpty())
+		reClaims, err := ValidateToken(token, pubKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reClaims).To(Equal(claims))
+	})
+
+	It("should be possible to load a RSA keys from files", func() {
+		ioutil.WriteFile("/tmp/rsa-priv.pem", rsaPrivKey, 0644)
+		ioutil.WriteFile("/tmp/rsa-pub.pem", rsaPubKey, 0644)
+		privKey, err := LoadPrivateKey("/tmp/rsa-priv.pem")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reflect.TypeOf(privKey).String()).To(Equal("*rsa.PrivateKey"))
+		pubKey, err := LoadPublicKey("/tmp/rsa-pub.pem")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reflect.TypeOf(pubKey).String()).To(Equal("*rsa.PublicKey"))
+	})
+
+	It("should be possible to load a ECDSA keys from files", func() {
+		ioutil.WriteFile("/tmp/ecdsa-priv.pem", ecdsaPrivKey, 0644)
+		ioutil.WriteFile("/tmp/ecdsa-pub.pem", ecdsaPubKey, 0644)
+		privKey, err := LoadPrivateKey("/tmp/ecdsa-priv.pem")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reflect.TypeOf(privKey).String()).To(Equal("*ecdsa.PrivateKey"))
+		pubKey, err := LoadPublicKey("/tmp/ecdsa-pub.pem")
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reflect.TypeOf(pubKey).String()).To(Equal("*ecdsa.PublicKey"))
+	})
+
+	It("should NOT be possible to load a keys from non-existing files", func() {
+		_, err := LoadPrivateKey("/tmp/not-here")
+		Expect(err).To(HaveOccurred())
+		_, err = LoadPublicKey("/tmp/not-here")
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("should NOT be possible to load a keys from files containing no key data", func() {
+		_, err := LoadPrivateKey("/etc/hosts")
+		Expect(err).To(HaveOccurred())
+		_, err = LoadPublicKey("/etc/hosts")
+		Expect(err).To(HaveOccurred())
+	})
+
+	It("should be possible to get a token from a http requests authorization header", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey, err := ParsePublicKey(rsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(rsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		r, _ := http.NewRequest("GET", "http://foobar.com", nil)
+		r.Header.Add("Authorization", "bearer "+token)
+		reClaims, err := GetClaimsFromRequest(r, pubKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(reClaims).To(Equal(claims))
+	})
+
+	It("should NOT be possible to get a token from a http requests authorization header if the header is malformed", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey, err := ParsePublicKey(rsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(rsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		r, _ := http.NewRequest("GET", "http://foobar.com", nil)
+		r.Header.Add("Authorization", token)
+		reClaims, err := GetClaimsFromRequest(r, pubKey)
+		Expect(err).To(HaveOccurred())
+		Expect(reClaims).To(BeEmpty())
+	})
+
+	It("should NOT be possible to create a token with a wrong key type", func() {
+		claims := Claims{"foo": "bar"}
+		token, err := CreateToken(claims, "no string key supported!")
+		Expect(err).To(HaveOccurred())
+		Expect(token).To(BeEmpty())
+	})
+
+	It("should NOT be possible to validate a token created with RSA with ECDSA key", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey, err := ParsePublicKey(ecdsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(rsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(token).NotTo(BeEmpty())
+		claims, err = ValidateToken(token, pubKey)
+		Expect(err).To(HaveOccurred())
+		Expect(claims).To(BeEmpty())
+	})
+
+	It("should NOT be possible to validate a token created with RSA with HMAC key", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey := []byte("foobar")
+		privKey, err := ParsePrivateKey(rsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(token).NotTo(BeEmpty())
+		claims, err = ValidateToken(token, pubKey)
+		Expect(err).To(HaveOccurred())
+		Expect(claims).To(BeEmpty())
+	})
+
+	It("should NOT be possible to validate a token created with ECDSA with RSA key", func() {
+		claims := Claims{"foo": "bar"}
+		pubKey, err := ParsePublicKey(rsaPubKey)
+		Expect(err).NotTo(HaveOccurred())
+		privKey, err := ParsePrivateKey(ecdsaPrivKey)
+		Expect(err).NotTo(HaveOccurred())
+		token, err := CreateToken(claims, privKey)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(token).NotTo(BeEmpty())
+		claims, err = ValidateToken(token, pubKey)
+		Expect(err).To(HaveOccurred())
+		Expect(claims).To(BeEmpty())
+	})
+
+})
+
+var (
+	rsaPubKey = []byte(`-----BEGIN CERTIFICATE-----
 MIIDAjCCAeqgAwIBAgIQZHmQHgRA7aLON9rpBmJ+XzANBgkqhkiG9w0BAQsFADAi
 MRAwDgYDVQQKEwdBY21lIENvMQ4wDAYDVQQDEwUuL3BraTAeFw0xODAxMDMwNzM4
 NThaFw0yODAxMDEwNzM4NThaMCIxEDAOBgNVBAoTB0FjbWUgQ28xDjAMBgNVBAMT
@@ -41,8 +188,8 @@ X5VSiSUyOQS/71Ld1YiolB/eJZjfxgBHQ4M6l/mQF59VA4aXZoJ/C7xXraNSvgMW
 MUhc9G4hykOg2I9FISY1ZdIngO6XI04zmFAeB9LzC8IfGtOJ5/3j9K6pus7sEuV7
 EmeHJzEi
 -----END CERTIFICATE-----
-`))
-		privKey, _ := ParsePrivateKey([]byte(`-----BEGIN RSA PRIVATE KEY-----
+`)
+	rsaPrivKey = []byte(`-----BEGIN RSA PRIVATE KEY-----
 MIIEpAIBAAKCAQEAsgOTJIJagCXATQQWF/EGOKRqwWhrQ+Cm0dlWz6MCoEGaBV2q
 i9THaBhdIk537tEy8yADeD9XFJP8dcnYdSZakUmJMVBvafFcy6o6WFrh/YT/a9Sc
 qQkQ7lPQAlO7wQSCH9ijmo/iECZNbXbt6+IHhccvENxT/hDycC+iX4cQKyiyBRGO
@@ -69,18 +216,8 @@ gPRqBL8v9GuOWUsQ/W/lCUJyNI+dkPYrv0++vyJedzrg3qPWCsOJfKODw888py8z
 vK1C9FdWsRyiJtDsBGLcDq+RUYg0kCWOtmLYpqlPbcOY7MueQ/rTSvit+AWR0Tfp
 lMSpb5DGvgUIn9NTvBcMThTU+h6Ay9IzyAobLsmXmqNa10zHOyFkqA==
 -----END RSA PRIVATE KEY-----
-`))
-		token, err := CreateToken(claims, privKey)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(token).NotTo(BeEmpty())
-		reClaims, err := ValidateToken(token, pubKey)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(reClaims).To(Equal(claims))
-	})
-
-	It("should be possible to create and verify a token using ECDSA", func() {
-		claims := Claims{"foo": "bar"}
-		pubKey, _ := ParsePublicKey([]byte(`-----BEGIN CERTIFICATE-----
+`)
+	ecdsaPubKey = []byte(`-----BEGIN CERTIFICATE-----
 MIIB7jCCAU+gAwIBAgIBATAKBggqhkjOPQQDBDAiMRAwDgYDVQQKEwdBY21lIENv
 MQ4wDAYDVQQDEwUuL3BraTAeFw0xODAxMTgxNDE4NDFaFw0yODAxMTYxNDE4NDFa
 MCAxEDAOBgNVBAoTB0FjbWUgQ28xDDAKBgNVBAMTA2lkcDCBmzAQBgcqhkjOPQIB
@@ -93,20 +230,13 @@ V2zUqImvjySvyCojUpvJ+K/djs/H5uOV7kHnKWkTYXJsDbpmKumADiWSUXQb/gJC
 Abzmio9Z2sezZ7hKDmmAkKpr8HfnYEOTR7RDzmELXxmE2qVr2K/BP23/JlgcSKo8
 4rLrCDTutW51Ufn2R9+xMBOG
 -----END CERTIFICATE-----
-`))
-		privKey, _ := ParsePrivateKey([]byte(`-----BEGIN EC PRIVATE KEY-----
+`)
+	ecdsaPrivKey = []byte(`-----BEGIN EC PRIVATE KEY-----
 MIHcAgEBBEIBJesj9eX4t8d2xdL+YqBqTi14heH5FIAP4KIMkfrxLhfm5mVVhPiJ
 U29K/3F/gP8722XvwUa4EFCc8afFRyGFi8igBwYFK4EEACOhgYkDgYYABAHvQcZP
 yizqQeF6fnALu0XTCyX7EHRKgzFM1Lq84IqzTyB1UxE3UzHVyMwSv5jmhjuWJk9f
 VlHAIMQpzKnWJOmWCwBrCpgbZZKi8+DCOxWzF0usB553CAwNsSoKoACKdxBjvdOX
 dxeHsy2+IlQC8vws0bHFi2nXSaYVO2qddtue9vmqeA==
 -----END EC PRIVATE KEY-----
-`))
-		token, err := CreateToken(claims, privKey)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(token).NotTo(BeEmpty())
-		reClaims, err := ValidateToken(token, pubKey)
-		Expect(err).NotTo(HaveOccurred())
-		Expect(reClaims).To(Equal(claims))
-	})
-})
+`)
+)
