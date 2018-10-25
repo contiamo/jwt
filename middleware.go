4082e50b4090371d36c1ba400aee0a881442664b
Tino Rusch <tino.rusch@gmail.com>
2018-10-25 12:19:09 +0200

    improve error logging in http middleware.

--- middleware.go
+++ middleware.go
@@ -2,7 +2,10 @@ package jwt
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 type claimContextKeyType string
@@ -27,19 +30,30 @@ func ClaimsToContextMiddleware(handler http.Handler, header string, idpKey inter
 
 // RequireClaim checks if the requests claims contain a specific value for a specific key
 func RequireClaim(handler http.Handler, claimKey, expectedClaimValue string) http.Handler {
+	log := logrus.WithField("require-claim", claimKey)
+	log = log.WithField("expected-value", expectedClaimValue)
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		claims := ClaimsFromContext(r.Context())
 		if claims == nil {
-			http.Error(w, "not authorized: failed to get token from context", http.StatusUnauthorized)
+			msg := "not authorized: failed to get token from context"
+			log.Debug(msg)
+			http.Error(w, msg, http.StatusUnauthorized)
 			return
 		}
+		log = log.WithFields("claims", claims)
 		claimVal, ok := claims[claimKey].(string)
 		if !ok {
-			http.Error(w, "not authorized: claim value has wrong type", http.StatusUnauthorized)
+			msg := "not authorized: claim value has wrong type"
+			log = log.WithField("actual-type", fmt.Sprintf("%T", claims[claimKey]))
+			log.Debug(msg)
+			http.Error(w, msg, http.StatusUnauthorized)
 			return
 		}
 		if claimVal != expectedClaimValue {
-			http.Error(w, "not authorized: calim value has unexpected content", http.StatusUnauthorized)
+			msg := "not authorized: claim value has unexpected content"
+			log = log.WithField("actual-content", claims[claimKey])
+			log.Debug(msg)
+			http.Error(w, msg, http.StatusUnauthorized)
 			return
 		}
 		handler.ServeHTTP(w, r)
